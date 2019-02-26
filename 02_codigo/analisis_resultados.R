edd64c8679f4c4859a3939ea5945f281aaad9b88
segasi <segasi@gmail.com>
2019-02-25 22:24:02 -0600

    Definir tema de gráficas

--- 02_codigo/analisis_resultados.R
+++ 02_codigo/analisis_resultados.R
@@ -7,6 +7,21 @@ Sys.setlocale("LC_ALL", "es_ES.UTF-8")
 options(scipen = 9999)
 theme_set(theme_gray())
 
+## Definir tema de gráficas ----
+tema <-  theme_minimal() +
+  theme(text = element_text(family="Didact Gothic Regular", color = "grey35"),
+        plot.title = element_text(size = 28, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 15),
+        panel.grid = element_line(linetype = 2), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(size = 16, face = "bold", family="Trebuchet MS Bold"),
+        legend.text = element_text(size = 14, family="Didact Gothic Regular"),
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
+        axis.text = element_text(size = 16, face = "bold", family="Didact Gothic Regular"))
+
 
 ### Importar shapefiles de municipios y estados ----
 
@@ -151,7 +166,6 @@ bd <-
 
 
 # Verificar que nombres de municipios sean iguales
-
 bd %>% 
   select(municipio, nombre_del_municipio, cve_edo_mpo) %>% 
   mutate(municipio = str_trim(municipio),
