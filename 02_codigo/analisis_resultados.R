cbdb3c1f92654820be899c3573eaa32b9e12fa9c
segasi <segasi@gmail.com>
2019-02-25 22:07:13 -0600

    Importar datos

--- 02_codigo/analisis_resultados.R
+++ 02_codigo/analisis_resultados.R
@@ -6,3 +6,12 @@ p_load(cowplot, janitor, lubridate, readxl, sf, tidyverse)
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
 options(scipen = 9999)
 theme_set(theme_gray())
+
+
+## Importar datos ----
+
+# Resultados de la consulta
+resultados <- read_csv("01_datos/resultado_mesas.csv")
+
+# Listado Nominal al 30 de septiembre de 2018
+ln <- read_excel("01_datos/EM_30_Sep.xlsx", range = "A1:E2492")
